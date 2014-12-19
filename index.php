3191cf68477d7adf4499fd0fdb32027801e3f456
Simmigon-flagg <simmigon.flagg@gmail.com>
2014-12-19 14:12:36 -0500

    Moves some stuff around
    This website will pull from the database so instead of HTML ext we will
    use php ext. The website will have HTML in it so don't worry.

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<!-- Jovy when you get the chance add another file called index.html -->
+<!-- I just changed the title -->
+<html>
+	<head>
+	<!-- This is how you put a css file into a web page. 
+	It's called an External Style Sheet-->
+		<link type="text/css" rel="stylesheet" href="stylesheet.css"/>
+		<title>Learn Tagalog Online</title> 
+	</head>
+	
+	<body>
+		<h1>Welcome to Learn Tagalog Online!</h1>
+			<p>
+					Learn to speak, read, and write Tagalog in no time!
+				Learn the basics and know some common Tagalog phrases and words to start a 
+				conversation today!
+			</p>
+	</body>
+</html>
